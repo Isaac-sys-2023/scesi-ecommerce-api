56f126cffff6eee7baf0300fef8f1f1c8a600c7b
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-08-29 23:52:34 -0400

    feat: Configure AppModule and TypeORM for Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => ({
+        type: 'postgres',
+        host: cfg.get('DB_HOST'),
+        port: cfg.get<number>('DB_PORT'),
+        username: cfg.get('DB_USER'),
+        password: cfg.get('DB_PASS'),
+        database: cfg.get('DB_NAME'),
+        autoLoadEntities: true,   // mueve entidades automáticamente
+        synchronize: true,        // SOLO en dev; en prod = false + migrations
+        logging: cfg.get('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
