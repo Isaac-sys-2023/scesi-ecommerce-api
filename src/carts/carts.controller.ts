73a2357a0a125df1a52cbb696774f2bf589f5afc
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-08-30 18:25:21 -0400

    feat: Create controllers for carts and orders modules

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { CartsService } from './carts.service';
+import { AddItemDto } from './dto/add-item.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-@Controller('carts')
-export class CartsController {}
+@ApiTags('cart')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'))
+@Controller('cart')
+export class CartsController {
+  constructor(private readonly service: CartsService) {}
+
+  @Get()
+  getCart(@Req() req: any) {
+    return this.service.getUserCart(req.user.userId);
+  }
+
+  @Post('add')
+  addItem(@Req() req: any, @Body() dto: AddItemDto) {
+    return this.service.addItem(req.user.userId, dto);
+  }
+}
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('orders')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'))
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private readonly service: OrdersService) {}
+
+  @Post('checkout')
+  checkout(@Req() req: any) {
+    return this.service.checkout(req.user.userId);
+  }
+
+  @Get()
+  myOrders(@Req() req: any) {
+    return this.service.findUserOrders(req.user.userId);
+  }
+}
