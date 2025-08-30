e9cdbde0857d4f7644c3edf631079e974b206582
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-08-29 23:53:15 -0400

    feat: Configure pipes, swagger and prefix in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('API para curso frontend (NestJS + Postgres)')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
