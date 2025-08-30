d2117ed848c702fb3274e2dd09eb32d6ccf79fe0
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-08-30 18:27:32 -0400

    feat: Update modules for carts and orders modules

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CartsController } from './carts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from './entities/cart.entity';
+import { CartItem } from './entities/cart-item.entity';
+import { UsersModule } from 'src/users/users.module';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cart, CartItem]), UsersModule, ProductsModule],
   providers: [CartsService],
-  controllers: [CartsController]
+  controllers: [CartsController],
+  exports: [CartsService],
 })
 export class CartsModule {}
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { CartsModule } from 'src/carts/carts.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order, OrderItem]), CartsModule],
   providers: [OrdersService],
-  controllers: [OrdersController]
+  controllers: [OrdersController],
 })
 export class OrdersModule {}
